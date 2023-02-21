d6fbd6c74040b0bfa380a80a6594be700cc5dd80
replaydk <98129711+replaydk@users.noreply.github.com>
2023-02-20 23:45:46 -0700

    Delete Week4 directory

--- Week4/dino class.js
+++
@@ -1,9 +0,0 @@
-
-class DinosaurC {
-    constructor(height, weight, color, species,){
-        this.height = height;
-        this.weight = weight;
-        this.color = color;
-        this.species = species; 
-    }
-}
