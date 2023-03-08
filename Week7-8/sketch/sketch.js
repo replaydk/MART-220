88f3911e95f45ad5a53db43c6b21503ef324ba21
replaydk <98129711+replaydk@users.noreply.github.com>
2023-03-08 13:55:00 -0700

    Add files via upload

---
+++ Week7-8/sketch/sketch.js
@@ -0,0 +1,14 @@
+// Have a character w/ Idle Animation
+// Walk Animation
+// Mirror on opposite way 
+// Classes that hold specific functions
+// Background Music # p.5sound
+// Have good food - and bad food
+// USE P.5 play 
+// WASD Keys to control movement 
+// immovable objects with collision w character 
+// score count
+// items that decrease health 
+// add health lose condition 
+// score win condition 
+// add new sprite sheet 
